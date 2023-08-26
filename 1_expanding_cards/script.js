3b57e7707e477047d0b8627a4119546337be1db3
Marta Fagúndez <martafagundez.dev@gmail.com>
2023-08-26 12:04:15 -0300

    My version of Expanding Cards

--- 1_expanding_cards/script.js
+++ 1_expanding_cards/script.js
@@ -1,14 +1,18 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".panel");
 
 panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        addActiveClass(panel);
+    });
+});
+
+function addActiveClass(element) {
+    element.classList.add("active");
+}
 
 function removeActiveClasses() {
     panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+        panel.classList.remove("active");
+    });
 }
