ad99f573186d2e78a24280a57f7472c6ef46c7c8
Marta Fagúndez <martafagundez.dev@gmail.com>
2023-08-31 09:21:53 -0300

    Scope adjusts for js files

--- 1_expanding_cards/script.js
+++ 1_expanding_cards/script.js
@@ -1,18 +1,20 @@
-const panels = document.querySelectorAll(".panel");
+{
+    const panels = document.querySelectorAll(".panel");
 
-panels.forEach(panel => {
-    panel.addEventListener("click", () => {
-        removeActiveClasses();
-        addActiveClass(panel);
-    });
-});
-
-function addActiveClass(element) {
-    element.classList.add("active");
-}
-
-function removeActiveClasses() {
     panels.forEach(panel => {
-        panel.classList.remove("active");
+        panel.addEventListener("click", () => {
+            removeActiveClasses();
+            addActiveClass(panel);
+        });
     });
+
+    function addActiveClass(element) {
+        element.classList.add("active");
+    }
+
+    function removeActiveClasses() {
+        panels.forEach(panel => {
+            panel.classList.remove("active");
+        });
+    }
 }
--- 2_progress_steps/script.js
+++ 2_progress_steps/script.js
@@ -1,58 +1,60 @@
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+{
+    const progress = document.getElementById("progress");
+    const prev = document.getElementById("prev");
+    const next = document.getElementById("next");
+    const circles = document.querySelectorAll(".circle");
 
-let currentActive = 0;
+    let currentActive = 0;
 
-next?.addEventListener("click", () => {
-    currentActive++;
+    next?.addEventListener("click", () => {
+        currentActive++;
 
-    if (currentActive > circles.length - 1) {
-        currentActive = circles.length - 1;
-    }
-
-    update(); 
-});
-
-prev?.addEventListener("click", () => {
-    currentActive--;
-
-    if (currentActive < 0) {
-        currentActive = 0;
-    }
-
-    update(); 
-});
-
-
-function update() {
-    circles.forEach((circle, index) => {
-        if(index <= currentActive) {
-            circle.classList.add("active");
-        } else {
-            circle.classList.remove("active");
+        if (currentActive > circles.length - 1) {
+            currentActive = circles.length - 1;
         }
 
-        if(index === currentActive) {
-            circle.classList.add("glowing");
-        } else {
-            circle.classList.remove("glowing");
-        }
+        update(); 
     });
 
-    const actives = document.querySelectorAll(".active");
+    prev?.addEventListener("click", () => {
+        currentActive--;
 
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + "%";
+        if (currentActive < 0) {
+            currentActive = 0;
+        }
 
-    if(currentActive === 0) {
-        prev.disabled = true;
-    }
-    else if (currentActive === circles.length - 1) {
-        next.disabled = true;
-    }
-    else {
-        prev.disabled = false;
-        next.disabled = false;
+        update(); 
+    });
+
+
+    function update() {
+        circles.forEach((circle, index) => {
+            if(index <= currentActive) {
+                circle.classList.add("active");
+            } else {
+                circle.classList.remove("active");
+            }
+
+            if(index === currentActive) {
+                circle.classList.add("glowing");
+            } else {
+                circle.classList.remove("glowing");
+            }
+        });
+
+        const actives = document.querySelectorAll(".active");
+
+        progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + "%";
+
+        if(currentActive === 0) {
+            prev.disabled = true;
+        }
+        else if (currentActive === circles.length - 1) {
+            next.disabled = true;
+        }
+        else {
+            prev.disabled = false;
+            next.disabled = false;
+        }
     }
 }
