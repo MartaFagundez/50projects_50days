48deff7cb13565b0f946a2ad33258c15c319adb5
Marta Fagúndez <martafagundez.dev@gmail.com>
2023-09-10 23:06:49 -0300

    Event Keycodes project: si detecta que el dispositivo es mobile, advierte de que se necesita teclado

--- 11-event-keycodes/script.js
+++ 11-event-keycodes/script.js
@@ -1,15 +1,20 @@
 const insert = document.getElementById('insert')
 
+// This checks if the current device is mobile
+if (navigator.userAgent.match(/Android/i)
+         || navigator.userAgent.match(/webOS/i)
+         || navigator.userAgent.match(/iPhone/i)
+         || navigator.userAgent.match(/iPad/i)
+         || navigator.userAgent.match(/iPod/i)
+         || navigator.userAgent.match(/BlackBerry/i)
+         || navigator.userAgent.match(/Windows Phone/i)) {
+          insert.innerHTML = `<div class="key">This website requires a keyboard to activate its functionality.</div>`;
+}
+
+
 window.addEventListener('keydown', (event) => {
   insert.innerHTML = `
-  <div class="key">
-  ${event.key === ' ' ? 'Space' : event.key} 
-  <small>event.key</small>
-</div>
-
-<div class="key">
-  ${event.code}
-  <small>event.code</small>
-</div>
-  `
+    <div class="key">${event.key === ' ' ? 'Space' : event.key} <small>event.key</small></div>
+    <div class="key">${event.code} <small>event.code</small></div>
+  `;
 })
