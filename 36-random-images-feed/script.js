d5d2a3ee7882d4d59b23e20aa256ecea73340dd7
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2023-10-13 08:27:09 -0300

    Random image feed version 2 done

--- 36-random-images-feed/script.js
+++ 36-random-images-feed/script.js
@@ -1,15 +1,23 @@
 {
-    const btn = document.getElementById("btn");
+  const btn = document.getElementById("btn");
 
-    btn?.addEventListener("click", function(){
-        location.reload();
-        return false;
-    });
-
-    // Inhabilitar el botón por 3 segundos para evitar que se hagan demasiadas peticiones seguidas a la api
+  btn?.addEventListener("click", () => {
     btn.disabled = true;
-    setTimeout(function() {
-        btn.disabled = false;
-    }, 3000);
 
+    document.body.classList.add("reloading");
+    // Desvanecer las imágenes antes de recargar la página
+    setTimeout(() => {
+      location.reload();
+    }, 500); // Recargar la página después de que la transición de opacidad haya terminado
+
+    // Mantener el botón inhabilitado por 5 segundos para evitar que se hagan demasiadas peticiones seguidas a la api
+    setTimeout(() => {
+      btn.disabled = false;
+    }, 5000);
+  });
+
+  // Devolver la opacidad completa a las imágenes cuando la página se recargue
+  window.addEventListener("beforeunload", () => {
+    document.body.classList.remove("reloading");
+  });
 }
